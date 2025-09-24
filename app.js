08dba31fbb89ecca459d7bd004c5b13eae32fa0e
temptationkentos57 <kreetlannoo@hotmail.com>
2025-09-24 16:03:49 +0200

    feat: initial commit for app.js

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Trang chính
+app.get('/', (req, res) => {
+  res.json({ message: 'Chào mừng đến với Giao Lưu Làm Việc Từ Xa' });
+});
+
+module.exports = app;
