760edd94633358e7c0bce181fca38f16eab9d958
temptationkentos57 <kreetlannoo@hotmail.com>
2025-09-24 16:04:27 +0200

    Updated greeting message for better clarity

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Chào mừng bạn đến với Giao Lưu Làm Việc Từ Xa!');
+  res.send('Welcome to Remote Work Collaboration!');
 });
 
 app.listen(PORT, () => {
-  console.log(`Server đang chạy trên cổng ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
